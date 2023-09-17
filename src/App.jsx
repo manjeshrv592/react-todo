79807b38ff674c9332e7a022604d568a4c118bc6
Manjesh R V <manjeshrv592@gmail.com>
2023-09-17 11:26:35 +0530

    Added form for new todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import NewTodoForm from './components/forms/NewTodoForm';
 
 const App = () => {
-  return <article className='card mx-auto shadow'>Hello React</article>;
+  return (
+    <article className='card mx-auto shadow'>
+      <NewTodoForm />
+    </article>
+  );
 };
 
 export default App;
---
+++ src/components/forms/NewTodoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const NewTodoForm = () => {
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // If no value in input return
+    if (!description) return;
+
+    // Generate unique ID
+    const newId = uuidv4();
+
+    const newTodo = { id: newId, description, completed: false };
+
+    console.log(newTodo);
+
+    // Clear input field
+    setDescription('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='new-todo-form'>
+      <input
+        type='text'
+        placeholder='Add new todo'
+        className='input'
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
+      <button className='btn btn-primary'>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodoForm;
